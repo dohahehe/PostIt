f6b237421e14c1cfb375fe51eb1bf164a1ccf01a
doha <doha3312ismail@gmail.com>
2026-01-18 15:39:34 +0200

    add heroui

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {HeroUIProvider} from "@heroui/react";
+import {Button} from "@heroui/button";
 
 function App() {
 
+
   return (
     <>
+    <HeroUIProvider>
       <div>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Button color="primary">Button</Button>;
       </div>
+    </HeroUIProvider>
     </>
   )
 }
