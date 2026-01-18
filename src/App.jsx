7a5dce8337e5e69f927bfc50392ae449083911ad
doha <doha3312ismail@gmail.com>
2026-01-18 15:46:02 +0200

    more packages

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import {HeroUIProvider} from "@heroui/react";
 import {Button} from "@heroui/button";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const queryClient = new QueryClient();
 function App() {
-
-
   return (
     <>
+    <QueryClientProvider client={queryClient}> 
     <HeroUIProvider>
       <div>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <Button color="primary">Button</Button>;
+        <Button color="primary">Button</Button>
       </div>
     </HeroUIProvider>
+    </QueryClientProvider>
     </>
   )
 }
